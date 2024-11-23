c5f9dffa760211a4d7c4d13a1e9f5514af87e2ea
Malcom Otieno <malcolmotieno16@gmail.com>
2024-11-23 21:04:47 +0300

    defeated

--- poll_app/polls/models.py
+++ poll_app/polls/models.py
@@ -8,7 +8,8 @@ from django.db import models
 from django.utils import timezone
 from datetime import datetime
 from django.contrib import admin
-
+# from datetime import datetime, timedelta
+import datetime
 # Create your models here.
 class Question(models.Model):
     """
@@ -55,3 +56,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
